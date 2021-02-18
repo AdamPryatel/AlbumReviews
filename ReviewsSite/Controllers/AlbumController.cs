31a0594186b3296f4a731e037ff0e8f9499cd148
jamar oneal <Cooler Master@DESKTOP-E7GOB8S>
2021-02-18 15:30:21 -0500

    added song list,song link folder for album links

--- ReviewsSite/Controllers/AlbumController.cs
+++ ReviewsSite/Controllers/AlbumController.cs
@@ -23,7 +23,7 @@ namespace ReviewsSite.Controllers
 
         public ViewResult Index()
         {
-            Album album = new Album(37, "The Black - Metallica", "The Black is Great!");
+            Album album = new Album(37, $"{}The Black - Metallica", " is the fifth studio album by American heavy metal band Metallica, released on August 12, 1991 through Elektra Records. It was recorded in an eight-month span at One on One Recording Studios in Los Angeles.");
             return View(album);
         }
 

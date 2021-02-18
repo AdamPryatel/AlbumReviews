2e43c64395fe30cdbf642175c28f9a5c0b4e8cc8
Adam Pryatel <adampryatel@gmail.com>
2021-02-18 13:17:46 -0500

    Added First Album

--- ReviewsSite/Controllers/AlbumController.cs
+++ ReviewsSite/Controllers/AlbumController.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using ReviewsSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+
+
 
 namespace ReviewsSite.Controllers
 {
     public class AlbumController : Controller
     {
+        //public Index()
+        //{
+        //    //return "Hello World!";
+
+        //    return View();
+
+        //}
+
+
         public ViewResult Index()
         {
-            //return "Hello World!";
-
-            return View();
-
+            Album album = new Album(37, "The Black - Metallica", "The Black is Great!");
+            return View(album);
         }
 
     }
---
+++ ReviewsSite/Models/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+
+        public Album(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+
+
+
+    }
+
+
+}

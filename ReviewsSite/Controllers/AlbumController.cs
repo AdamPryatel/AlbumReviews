8d2b03288660a7bbec0592845fb225cea24a633c
Adam Pryatel <adampryatel@gmail.com>
2021-02-19 14:11:20 -0500

    Added Repository Class

--- ReviewsSite/Controllers/AlbumController.cs
+++ ReviewsSite/Controllers/AlbumController.cs
@@ -21,11 +21,23 @@ namespace ReviewsSite.Controllers
         //}
 
 
+
         public ViewResult Index()
         {
-            Album album = new Album(37, $"{}The Black - Metallica", " is the fifth studio album by American heavy metal band Metallica, released on August 12, 1991 through Elektra Records. It was recorded in an eight-month span at One on One Recording Studios in Los Angeles.");
-            return View(album);
+            Album album = new Album(1, "The Black", "Is the fifth studio album by American heavy metal band Metallica, released on August 12, 1991 through Elektra Records. It was recorded in an eight-month span at One on One Recording Studios in Los Angeles.", "Metallica");
+
+            Album album2 = new Album(2, "Man on the Moon 3", "Is the seventh studio album by American rapper Kid Cudi. It was released on December 11, 2020 by Republic Records. It is the final installment of Cudi's Man on the Moon trilogy of albums.", "Kid Cudi");
+;
+
+            return View();
+
+
         }
+
+        //public ViewResult Index()
+        //{
+
+        //}
 
     }
 }
--- ReviewsSite/Models/Album.cs
+++ ReviewsSite/Models/Album.cs
@@ -7,24 +7,29 @@ namespace ReviewsSite.Models
 {
     public class Album
     {
+
+        public List<Album> ListofAlbums = new List<Album>();
+
+
         public int Id { get; set; }
 
         public string Name { get; set; }
 
         public string Description { get; set; }
 
-        public string Image { get; set; }
+        public string BandName { get; set; }
         
         public string Category { get; set; }
 
         public string Review { get; set; }
 
 
-        public Album(int id, string name, string description)
+        public Album(int id, string name, string description, string bandName)
         {
             Id = id;
             Name = name;
             Description = description;
+            BandName = bandName;
         }
 
 

bf92785063c00e7717805adf11ba3c14fa9669fe
Adam Pryatel <adampryatel@gmail.com>
2021-02-23 13:55:07 -0500

    Added Context Class

---
+++ ReviewsSite/Repositories/AlbumRepository.cs
@@ -0,0 +1,32 @@
+﻿using ReviewsSite.Controllers;
+using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite
+{
+    public class AlbumRepository : AlbumController
+    {
+        //public List<Album> ListofAlbums = new List<Album>();
+
+        //public void AddAlbum(Album album)
+        //{
+        //    ListofAlbums.Add(album);
+        //}
+
+        ////public void SelectAlbum()       // public Album SelectAlbum()
+        ////{
+        ////    int id = 1;
+
+        ////    Console.WriteLine("Please select a Album ID: ");
+        ////}
+
+
+
+
+
+
+    }
+}

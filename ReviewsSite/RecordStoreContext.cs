688a6fb9243437b332eadf1eaae73fe0a1a69f57
Michael Romantic <michaeljromantic@gmail.com>
2021-03-04 17:11:28 -0500

    added other teams songs

--- ReviewsSite/RecordStoreContext.cs
+++ ReviewsSite/RecordStoreContext.cs
@@ -101,6 +101,33 @@ namespace ReviewsSite
                      Description = "A Moment Apart is the third studio album by the American electronic music duo ODESZA, released on 8 September 2017 through Counter, Ninja Tune and the duo's own label, Foreign Family Collective. It is the duo's first album in three years after their second album, In Return, and the first released through Foreign Family Collective.",
                      SongsString = "Intro, A Moment Apart, Higher Ground, Boy, Line of Sight, Late Night, Across the Room, Meridian, Everything at your feet, Just a memory, Divide, Thin Floors and Tall Cielings, La Ciudad, Falls, Show Me, Corners of the Earth",
                      Image = "Images/amap.jfif"
+                 },
+                 new Album()
+                 {
+                     Id = 10,
+                     Name = "Original Sound Track",
+                     BandName = "The Shining",
+                     Description = "The Shining is a 1980 psychological horror film produced and directed by Stanley Kubrick and co-written with novelist Diane Johnson. The film is based on Stephen King's 1977 novel of the same name and stars Jack Nicholson, Shelley Duvall, Scatman Crothers, and Danny Lloyd.",
+                     SongsString = "The Shining - Main Title, Rocky Mountains, Lontano, Music for Strings Percussion And Celesta, Utrenja (Exerpt), The Awakening Of Jacob, De Natura Sonoris No. 2, Home",
+                     Image = "Images/TheShining.jpg"
+                 },
+                 new Album()
+                 {
+                     Id = 11,
+                     Name = "Hamilton",
+                     BandName = "Lin-Manuel Miranda",
+                     Description = "Hamilton is the cast album to the 2015 musical Hamilton. The musical is based on the 2004 biography of Alexander Hamilton written by Ron Chernow, with music, lyrics, and book by Lin-Manuel Miranda.",
+                     SongsString = "Alexander Hamilton, Aaron Burr Sir, My Shot, The Story Of Tonight, The Schuyler Sisters, Farmer Refuted, You'll Be Back, Right Hand Man, A Winter's Ball , Helpless, Satisfied, The Story of Tonight (Reprise), Wait For It, Stay Alive, Ten Duel Commandments, Meet Me Inside, That Would Be Enough Guns and Ships, History Has Its Eyes On You, Yorktown (The World Turned Upside Down), What Comes Next, Dear Theodosia, Non-Stop",
+                     Image = "Images/Hamilton.jpg"
+                 },
+                 new Album()
+                 {
+                     Id = 12,
+                     Name = "We Like Pizza",
+                     BandName = "Pizza Kids",
+                     Description = "We Like Pizza by the Pizza Kids from the album We Like Pizza. In this song they highlight many of the wonders of the Italian dish, pizza, They sing about the toppings and the time of day that they like their pizza. It is shown that these children have a strange obsession to the pizza and worship it on a daily basis, in the morning and in the evening. It is a very modern song for it’s time because it underlines many things that children enjoy, such as pizza.",
+                     SongsString = "We Like Pizza (Frozen Version), We Like Pizza (Happy Version), We Like Pizza (Waves Version)",
+                     Image = "Images/Pizza.jpg"
                  }
                  );
 
